a6bd3b0e2e375d9b4dc24618e8280a804b2eed82
PhuongDoz <38605217+PhuongDoz@users.noreply.github.com>
2018-04-26 15:58:27 +0700

    QuanLyCaoOc-01

---
+++ QLyCaoOc/QLyCaoOc/Controllers/BuildingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLyCaoOc.Models;
+
+namespace QLyCaoOc.Controllers
+{
+    public class BuildingController : Controller
+    {
+        dbQLyCaoOcDataContext data = new dbQLyCaoOcDataContext();
+        // GET: Building
+        private List<HOPDONG> ListHopDong(int count)
+        {
+            return data.HOPDONGs.OrderByDescending(a => a.NgayLap).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            var hopdongmoi =  ListHopDong(50);
+
+            return View(hopdongmoi);
+        }
+    }
+}
---
+++ QLyCaoOc/QLyCaoOc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QLyCaoOc.MvcApplication" Language="C#" %>
