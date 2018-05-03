abd33915feb02f61c002996784653e1bd2472c66
PhuongDoz <38605217+PhuongDoz@users.noreply.github.com>
2018-05-03 22:05:07 +0700

    QuanLyCaoOc-01.1.2

--- QLyCaoOc/QLyCaoOc/Controllers/ContractController.cs
+++ QLyCaoOc/QLyCaoOc/Controllers/ContractController.cs
@@ -5,21 +5,43 @@ using System.Web;
 using System.Web.Mvc;
 using QLyCaoOc.Models;
 
+
 namespace QLyCaoOc.Controllers
 {
     public class ContractController : Controller
     {
-        dbQLyCaoOcDataContext data = new dbQLyCaoOcDataContext();
+        dbQLyCaoOcDataContext db = new dbQLyCaoOcDataContext();
         // GET: Building
         private List<HOPDONG> ListHopDong(int count)
         {
-            return data.HOPDONGs.OrderByDescending(a => a.NgayLap).Take(count).ToList();
+            return db.HOPDONGs.OrderByDescending(a => a.NgayLap).Take(count).ToList();
         }
         public ActionResult Index()
         {
+          
             var hopdongmoi =  ListHopDong(200);
-
             return View(hopdongmoi);
         }
+        [HttpGet]
+        public ActionResult Themhopdong()
+        {
+            return View();
+        }
+        public ActionResult Chitiethopdong(string id)
+        {
+            HOPDONG hopdong = db.HOPDONGs.SingleOrDefault(n => n.MaHD == id);
+            ViewBag.MaHD = hopdong.MaHD;
+            if (hopdong == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(hopdong);
+
+
+        
+        }
+
+       
     }
 }
---
+++ QLyCaoOc/QLyCaoOc/Models/ContractViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLyCaoOc.Models
+{
+    public class ContractViewModel
+    {
+        public CHITIETHOPDONG cthd { get; set; }
+        public TANG tang { get; set; }
+        public HOPDONG hd { get; set; }
+        
+    }
+    public class HopDong
+    {
+        public string MaHD { get; set; }
+        public DateTime NgayLap { get; set; }
+        public int TienCoc { get; set; }
+        public string MaPHG { get; set; }
+        public string TenPHG { get; set; }
+        public int Tang { get; set; }
+        public int DienTich { get; set; }
+        public int Gia { get; set; }
+        public int ThoiGianThue { get; set; }
+    }
+}
